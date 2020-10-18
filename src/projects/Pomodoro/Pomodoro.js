f841ad9b8e044c87bd3ecfdf0bf38d89a30947be
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-18 16:02:56 -0400

    Implemented app logic and added styling

--- src/projects/Pomodoro/Pomodoro.js
+++ src/projects/Pomodoro/Pomodoro.js
@@ -1,29 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Pomodoro.css";
 
 const padTime = (time) => time.toString().padStart(2, "0");
 
 const Pomodoro = () => {
     // State
-    const [timeLeft, setTimeLeft] = useState(3);
+    const [timeLeft, setTimeLeft] = useState(25 * 60);
     const [title, setTitle] = useState("Let the countdown begin!");
+    const [isRunning, setIsRunning] = useState(false);
+
+    // To persist state/data between renders, use the
+    // useRef hook
+    const intervalRef = useRef(null);
 
     // helper functions
     const startTimer = () => {
-        setInterval(() => {
+        setIsRunning(true);
+        setTitle("You're doing great!");
+        intervalRef.current = setInterval(() => {
             setTimeLeft((timeLeft) => {
                 if (timeLeft >= 1) {
                     return timeLeft - 1;
                 }
+                resetTimer();
                 return 0;
             });
         }, 1000);
+    };
+
+    const stopTimer = () => {
+        setIsRunning(false);
+        setTitle("Keep Going!");
+        clearInterval(intervalRef.current);
+    };
+
+    const resetTimer = () => {
+        clearInterval(intervalRef.current);
+        setIsRunning(false);
+        setTitle("Click start to go again");
+        setTimeLeft(25 * 60);
     };
 
     // Convert time to Minutes & Seconds
     const minutes = padTime(Math.floor(timeLeft / 60));
     const seconds = padTime(Math.floor(timeLeft - minutes * 60));
 
+    // Render
     return (
         <div className="pomodoro">
             <h2>{title}</h2>
@@ -35,9 +57,13 @@ const Pomodoro = () => {
             </div>
 
             <div className="pomodoro-buttons">
-                <button onClick={startTimer}>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                <button onClick={startTimer} disabled={isRunning}>
+                    Start
+                </button>
+                <button onClick={stopTimer} disabled={!isRunning}>
+                    Stop
+                </button>
+                <button onClick={resetTimer}>Reset</button>
             </div>
         </div>
     );
