46de2480f436fb4ef4afe879ef0fb853632c0734
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-18 18:14:52 -0400

    Implemented markdown conversion logic

--- src/projects/Markdown/Markdown.js
+++ src/projects/Markdown/Markdown.js
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
+import unified from "unified";
+import parse from "remark-parse";
+import remark2react from "remark-react";
+
 import "./Markdown.css";
 
-const Markdown = () => (
-    <div className="Markdown">
-        <textarea className="Markdown-textarea" />
+const Markdown = () => {
+    const [markdown, setMarkdown] = useState("## Start typing here");
+    return (
+        <div className="Markdown">
+            {/* Display textbox where user types in */}
+            <textarea
+                className="Markdown-textarea"
+                value={markdown}
+                onChange={(e) => setMarkdown(e.target.value)}
+            />
 
-        <div className="Markdown-preview"></div>
-    </div>
-);
+            {/* Display text converted to markdown*/}
+            <div className="Markdown-preview">
+                {
+                    unified().use(parse).use(remark2react).processSync(markdown)
+                        .result
+                }
+            </div>
+        </div>
+    );
+};
 
 export default Markdown;
