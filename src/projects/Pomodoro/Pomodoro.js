1e71ff2580b25629e31fbbe19cfff83931908427
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-18 15:18:57 -0400

    Added Pomodoro app and implemented basic count-down logic

--- src/projects/Pomodoro/Pomodoro.js
+++ src/projects/Pomodoro/Pomodoro.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Pomodoro.css";
 
+const padTime = (time) => time.toString().padStart(2, "0");
+
 const Pomodoro = () => {
+    // State
+    const [timeLeft, setTimeLeft] = useState(3);
+    const [title, setTitle] = useState("Let the countdown begin!");
+
+    // helper functions
+    const startTimer = () => {
+        setInterval(() => {
+            setTimeLeft((timeLeft) => {
+                if (timeLeft >= 1) {
+                    return timeLeft - 1;
+                }
+                return 0;
+            });
+        }, 1000);
+    };
+
+    // Convert time to Minutes & Seconds
+    const minutes = padTime(Math.floor(timeLeft / 60));
+    const seconds = padTime(Math.floor(timeLeft - minutes * 60));
+
     return (
         <div className="pomodoro">
-            <h2>Pomodoro!</h2>
+            <h2>{title}</h2>
 
             <div className="pomodoro-timer">
-                <span>00</span>
+                <span>{minutes}</span>
                 <span>:</span>
-                <span>00</span>
+                <span>{seconds}</span>
             </div>
 
             <div className="pomodoro-buttons">
-                <button>Start</button>
+                <button onClick={startTimer}>Start</button>
                 <button>Stop</button>
                 <button>Reset</button>
             </div>
