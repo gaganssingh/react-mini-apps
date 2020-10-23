bb0548f99c0abe2e8f54bb91b3959a32938b46f8
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-23 19:47:09 -0400

    Implemented protected and unprotected routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
+import LoginPage from "./components/LoginPage/LoginPage";
 import "./App.css";
 
 // Lazy-Loaded Components
@@ -16,24 +18,38 @@ const RockPaperScissors = React.lazy(() =>
 );
 
 const App = () => {
+    const { isAuthenticated } = useAuth0();
+    console.log("App -> isAuthenticated", isAuthenticated);
+
+    const protectedRoutes = (
+        <Switch>
+            <React.Suspense fallback={<h2>Loading...</h2>}>
+                <Route exact path="/" component={HomeScreen} />
+                <Route path="/pomodoro" component={Pomodoro} />
+                <Route path="/markdown" component={Markdown} />
+                <Route path="/minizelda" component={MiniZelda} />
+                <Route path="/infinitely" component={Infinitely} />
+                <Route path="/quiz" component={QuizMe} />
+                <Route
+                    path="/rockpaperscissors"
+                    component={RockPaperScissors}
+                />
+            </React.Suspense>
+        </Switch>
+    );
+
+    const unprotectedRoutes = (
+        <Switch>
+            <React.Suspense fallback={<h2>Loading...</h2>}>
+                <Route exact path="/" component={LoginPage} />
+            </React.Suspense>
+        </Switch>
+    );
+
     return (
         <Router>
             <Header />
-            <Switch>
-                <React.Suspense fallback={<h2>Loading...</h2>}>
-                    <Route exact path="/" component={HomeScreen} />
-                    <Route path="/pomodoro" component={Pomodoro} />
-                    <Route path="/markdown" component={Markdown} />
-                    <Route path="/minizelda" component={MiniZelda} />
-                    <Route path="/infinitely" component={Infinitely} />
-                    <Route path="/quiz" component={QuizMe} />
-
-                    <Route
-                        path="/rockpaperscissors"
-                        component={RockPaperScissors}
-                    />
-                </React.Suspense>
-            </Switch>
+            {isAuthenticated ? protectedRoutes : unprotectedRoutes}
         </Router>
     );
 };
---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./LoginPage.css";
+
+const LoginPage = () => {
+    return (
+        <div className="LoginPage">
+            <h1>Login Page</h1>
+        </div>
+    );
+};
+
+export default LoginPage;
